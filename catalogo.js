9fcac5a7d0cdbabb8ac3054d752cc3960c16b017
Showto <marsegovia@frba.utn.edu.ar>
2025-04-02 21:46:02 -0300

    nueva pag

---
+++ catalogo.js
@@ -0,0 +1,22 @@
+fetch("catalogo.json")
+.then(response => response.json())
+.then(data => {
+    const contenedor = document.getElementById("productos2");
+    data.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+
+        const img = document.createElement("img");
+        img.src = producto.imagen;
+        img.alt = producto.nombre;
+
+        const nombre = document.createElement("p");
+        nombre.textContent = producto.nombre;
+
+        div.appendChild(img);
+        div.appendChild(nombre);
+
+        contenedor.appendChild(div);
+    });
+})
+.catch(error => console.error("Error cargando los productos:", error));
