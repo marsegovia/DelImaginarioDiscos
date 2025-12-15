3d82afce6d7ae00ed4ef4be6dae1ba48754d96cb
Martin Segovia <marsegovia@frba.utn.edu.ar>
2025-12-15 11:08:56 -0300

    Firebas cambiado

--- BackEnd/src/data/firebaseAdmin.js
+++ BackEnd/src/data/firebaseAdmin.js
@@ -1,19 +1,21 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(serviceAccountPath, "utf8")
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: "service_account",
+      project_id: process.env.FB_PROJECT_ID,
+      private_key_id: process.env.FB_PRIVATE_KEY_ID,
+      private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.FB_CLIENT_EMAIL,
+      client_id: process.env.FB_CLIENT_ID,
+      auth_uri: process.env.FB_AUTH_URI,
+      token_uri: process.env.FB_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT,
+      client_x509_cert_url: process.env.FB_CLIENT_CERT
+    })
+  });
+}
 
 export const adminAuth = admin.auth();
+export default admin;
